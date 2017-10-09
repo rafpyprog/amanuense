74d05475f00b94e11a0522a6683fd6412b288135
rafael <rafael.alves.ribeiro@gmail.com>
2017-10-09 15:24:56 -0300

    Adicionado ao Pypi

--- README.rst
+++ README.rst
@@ -4,8 +4,8 @@ Parser do Diário Oficial da União (D.O.U)
 #Tutorial
 
 
-    from amanuense import Diario
-    
+    from amanuense.diario import Diario
+
     pdf = '/home/rafael/Área de Trabalho/cartesiano/tests/data/DOUS3.pdf'
     diario = Diario(pdf)
 
@@ -13,8 +13,6 @@ Parser do Diário Oficial da União (D.O.U)
 
     # Sumário do D.O.U conforme apresentado na primeira página do documento
     diario.summary
-    
+
     # Retorna o conteúdo publicado por um determinado órgão do Governo
     diario.section_contents('Presidência da República').splitlines()
-    
-    
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup
+
+from amanuense.__init__ import __version__
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='amanuense',
+    version=__version__,
+    author='Rafael Alves Ribeiro',
+    author_email='rafael.alves.ribeiro@gmail.com',
+    description='Parser do Diário Oficial da União (DOU)',
+    license='MIT',
+    keywords = 'parser dou',
+    url = 'https://github.com/rafpyprog/amanuense',
+    packages=['amanuense'],
+    install_requires=['lxml',],
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
